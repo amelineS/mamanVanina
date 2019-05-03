b42f4ea2c569608e8bb01aef8c62131150fd77c7
LudoPilou <guerlesquin.ludovic@gmail.com>
2019-05-03 20:48:01 +0200

    Premier commit Ludo

--- src/main/java/com/vanina/gestionVanina/GestionVaninaApplication.java
+++ src/main/java/com/vanina/gestionVanina/GestionVaninaApplication.java
@@ -2,12 +2,22 @@ package com.vanina.gestionVanina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.vanina.gestionVanina.dao.IProduitDAO;
+import com.vanina.gestionVanina.entities.Produit;
 
 @SpringBootApplication
 public class GestionVaninaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionVaninaApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(GestionVaninaApplication.class, args);
+		IProduitDAO produitRepo = ctx.getBean(com.vanina.gestionVanina.dao.IProduitDAO.class);
+		
+		
+		Produit produit1=new Produit(null, "vanina", "L", "rouge", "T-shirt", 45f);
+		
+		produit1=produitRepo.save(produit1);
 	}
 
 }
--- src/main/java/com/vanina/gestionVanina/entities/Produit.java
+++ src/main/java/com/vanina/gestionVanina/entities/Produit.java
@@ -1,5 +1,36 @@
 package com.vanina.gestionVanina.entities;
 
-public class Produit {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Produit implements Serializable {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1563129641322307003L;
+	
+	@Id @GeneratedValue
+	private Long idProduit;
+	private String fabricant;
+	private String taille;
+	private String couleur;
+	private String modele;
+	private float prix;
+	
+	
+	
 }
